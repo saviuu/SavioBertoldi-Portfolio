cfaf63882a5f42d72eccf6254bc5fa68a4d40322
Sávio Bertoldi <savio.b09@gmail.com>
2024-08-01 16:22:20 -0300

    Iniciando projeto portfólio.
    Atualizado o portfólio pessoal com diversas melhorias e novas seções.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { APP_TEXT } from './constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, // Marcar como standalone
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'SavioBertoldi-Portfolio';
+  title = APP_TEXT.title;
+  about = APP_TEXT.about;
+  experience = APP_TEXT.experience;
+
+  skills = [
+    '.NET', 'C#', 'TypeScript', 'Oracle PL/SQL', 'GraphQL', 'Angular', 'JavaScript',
+    'Git', 'Clean code', 'HTML', 'GitHub', 'IIS', 'CSS', 'Web Development',
+    'Backend Development', 'Full-Stack Development'
+  ];
+  email = APP_TEXT.email;
+  linkedin = APP_TEXT.linkedIn
+
 }
---
+++ src/app/constants/app-text.constants.ts
@@ -0,0 +1,161 @@
+// src/app/app-text.constants.ts
+
+export const APP_TEXT = {
+  title: 'Portfólio Sávio Bertoldi',
+  email: 'savio.b09@gmail.com',
+  linkedIn: 'https://www.linkedin.com/in/sávio-bertoldi-2b09a51b9/',
+  about:
+                  `Atualmente, atuo como Desenvolvedor Web Full-stack, trazendo uma sólida expertise em
+                    Angular e TypeScript para o desenvolvimento Front-end, enquanto foco intensamente no
+                    desenvolvimento Backend utilizando C#, .NET e Oracle PL/SQL.`,
+  experience:
+    [
+      {
+        position: 'Desenvolvedor Web Full-stack',
+        company: 'Operacional.com',
+        period: 'Agosto 2021 | Presente',
+        description: `  Meu conhecimento avançado em TypeScript e no framework Angular 13+ me permite realizar
+                          desenvolvimento e implementações de novas funcionalidades e componentes, utilizando
+                          uma library interna personalizada baseada nos componentes da DevExtreme. Criando
+                          interfaces de usuário dinâmicas e responsivas, essa habilidade garante que eu possa
+                          fornecer soluções robustas e eficientes, adaptadas às necessidades específicas de cada
+                          projeto.
+                          No Backend, meu foco principal é o uso de .NET e C#, onde desenvolvo APIs e serviços
+                          robustos que garantem a integração perfeita entre o Frontend e o Backend. Implemento
+                          soluções escaláveis e eficientes, adequadas para atender às complexas necessidades
+                          dos projetos. Utilizo uma Arquitetura Modular em Camadas com Padrão Repository e
+                          RESTful APIs no backend, o que assegura uma organização clara e eficiente das
+                          responsabilidades do sistema. Tenho conhecimento profundo em estrutura de entidade,
+                          utilizando Dapper para acesso eficiente a dados, e aplico os princípios de orientação a
+                          objetos para criar sistemas modulares e de fácil manutenção. Trabalho com as versões 6,
+                          7 e 8 do .NET, mantendo-me atualizado com as últimas melhorias e funcionalidades da
+                          plataforma. Além disso, sigo o padrão RESTful para APIs, assegurando que meus serviços
+                          sejam bem estruturados, facilmente consumíveis e compatíveis com os padrões da
+                          indústria. Tenho experiência com implementações, desenvolvimentos e uso de
+                          WebSockets em C# utilizando a biblioteca SignalR.
+                          Minha experiência em banco de dados Oracle PL/SQL complementa este trabalho,
+                          permitindo-me otimizar consultas e gerenciar dados de forma eficiente, assegurando que
+                          as aplicações sejam não apenas funcionais, mas também performáticas e escaláveis.
+                          Experiência estendida à manutenção e correção de sistemas, onde aplico uma análise
+                          detalhada da estrutura do sistema e do código para identificar e resolver problemas de
+                          forma eficaz. Essa capacidade analítica também me permite conduzir análises para
+                          desenvolver novas implementações no sistema, auxiliando diversos membros da equipe e
+                          proporcionando orientações que facilitam o desenvolvimento geral e melhoram a
+                          eficiência do time.
+                          Tenho vasta experiência na aplicação e implementação de regras de negócio, adaptando
+                          soluções para atender às demandas específicas de cada cliente. Também possuo
+                          conhecimentos avançados em geração de versões e build final de aplicações para
+                          hospedagem em IIS. Minha trajetória profissional inclui conhecimentos avançados em
+                          servidor IIS e na migração e desenvolvimento de Sistemas ERP em Delphi para WEB.
+                          Atualmente, sou responsável pela hospedagem e atualização dos sistemas ERP web de
+                          nossos clientes utilizando a plataforma de nuvem Microsoft Azure. Minha experiência
+                          inclui a configuração e gerenciamento do Azure App Service para garantir a alta
+                          disponibilidade e escalabilidade dos aplicativos. Essa experiência me proporcionou um
+                          profundo entendimento do ciclo de vida completo de aplicações web em ambiente de
+                          nuvem, desde o desenvolvimento e testes até a implantação e manutenção contínua,
+                          sempre buscando otimizar o desempenho e garantir a segurança e confiabilidade dos
+                          sistemas.
+                          Tenho uma forte habilidade em comunicação e trabalho em equipe, o que me permite
+                          colaborar eficazmente com colegas e stakeholders, garantindo que os projetos atendam
+                          às expectativas e sejam entregues dentro dos prazos estabelecidos. Adicionalmente,
+                          possuo vasta experiência em lidar diretamente com clientes, entendendo suas
+                          necessidades e proporcionando soluções que agregam valor aos seus negócios.
+                          Um dos meus orgulhos profissionais é a experiência em treinar e orientar estagiários na
+                          área de desenvolvimento web, ajudando a formar a próxima geração de desenvolvedores.
+                          Este papel de mentor reforça meu compromisso com a aprendizagem contínua e o
+                          compartilhamento de conhecimento, que são essenciais para o crescimento e inovação
+                          na área de tecnologia.
+                          Comprometido com o aprendizado contínuo, estou constantemente aprimorando minhas
+                          habilidades e adquirindo novos conhecimentos para estar sempre alinhado com as
+                          necessidades do mercado de trabalho. Minha dedicação ao desenvolvimento profissional
+                          assegura que estou sempre preparado para enfrentar novos desafios e contribuir de
+                          maneira significativa para o sucesso dos projetos em que estou envolvido.
+                          Após finalizar a graduação tecnólogo em Análise e Desenvolvimento de Sistemas, tenho a
+                          ambição de cursar a pós-graduação em Engenharia de Software. Acredito que essa
+                          especialização irá me proporcionar um conhecimento ainda mais profundo e
+                          especializado na área, permitindo-me contribuir de forma mais significativa para o
+                          desenvolvimento de projetos complexos e inovadores.
+                          Atualmente, estou estudando inglês com o objetivo de alcançar fluência na língua,
+                          expandindo minhas habilidades de comunicação e tornando-me ainda mais eficaz em um
+                          ambiente global.
+
+                          • Experiência de 3 anos como Desenvolvedor Web Full-stack, Especialista em Angular,
+                          TypeScript, C#, .NET e Oracle PL/SQL.
+
+                          • Implementação de funcionalidades e melhorias em sistemas utilizando tecnologias web.
+
+                          • Manutenção de sistemas e resolução de problemas utilizando Angular, TypeScript, C#,
+                          .NET e Oracle PL/SQL.
+
+                          • Participação em projetos de desenvolvimento full-stack com foco em Angular e C#.
+
+                          • Análise e otimização de consultas em bancos de dados Oracle PL/SQL.
+
+                          • Integração de sistemas e APIs utilizando tecnologias como Angular, TypeScript e .NET.
+
+                          • Desenvolvimento de soluções customizadas em C# para atender demandas específicas.
+
+                          • Participação em processos de deploy e testes de aplicações web desenvolvidas.
+
+                          • Domínio em design responsivo e experiência do usuário.
+
+                          • Experiência em versionamento de código Git.
+
+                          • Participação em projetos complexos e de alta performance.
+
+                          • Capacidade de trabalhar em equipe e resolver problemas de forma eficiente.
+
+                          • Experiência na integração de sistemas e bancos de dados.
+
+                          • Proficiente em desenvolvimento back-end com .NET e Oracle PL/SQL.
+
+                          • Experiência com administração e configuração de servidores Windows Server
+
+                          • Conhecimento em troubleshooting e resolução de problemas em servidores IIS
+
+                          • Participação em projetos de migração de dados e sistemas.
+
+                          • Experiência na análise e resolução de problemas em sistemas, garantindo a
+                          estabilidade e segurança das operações.
+
+                          • Experiência em auxiliar membros da equipe na realização de tarefas diárias.
+
+                          • Conhecimento em trabalhar em conjunto com colegas para atingir objetivos comuns.
+
+                          • Habilidade em apoiar e orientar novos colaboradores durante o processo de
+                          integração.
+
+                          • Experiência em lidar com situações de alta pressão e manter a calma sob estresse.
+
+                          • Experiência em integração de serviços e APIs com Azure App Service.
+
+                          • Conhecimento em implementação e manutenção de infraestrutura em nuvem Azure
+`
+      },
+      {
+        position: 'Eletricista Industrial',
+        company: 'L & R Instalações elétricas',
+        period: 'Janeiro 2018 | Agosto 2021',
+        description: `Sou formado pelo Senai em Instalações Elétricas Prediais, acumulando mais de 3 anos de
+                        experiência prática como eletricista. Durante esse período, executei instalações elétricas
+                        complexas, solucionei problemas técnicos variados e desenvolvi análises detalhadas para
+                        assegurar a entrega de serviços de alta qualidade aos clientes. Essa experiência inicial
+                        formou a base para a minha transição bem-sucedida para a área de tecnologia, onde
+                        aplico minha atenção aos detalhes e habilidades analíticas para resolver desafios de
+                        desenvolvimento de software.
+
+                          • Experiência em instalação e manutenção de sistemas elétricos industriais
+
+                          • Conhecimento em leitura e interpretação de diagramas elétricos
+
+                          • Capacidade de identificar e resolver problemas em circuitos elétricos
+
+                          • Trabalhou com equipamentos eletromecânicos em ambiente industrial
+
+                          • Realizou testes de funcionamento em painéis elétricos
+
+                        `
+      }
+  ]
+
+};
